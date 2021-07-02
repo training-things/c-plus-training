fd7403ecb487220427b220b00db9dec1f2390be5
aierui <aieruishi@gmail.com>
2021-07-03 00:02:52 +0800

    c

---
+++ src/assert/assert.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main()
+{
+    int x = 7;
+
+    x = 9;
+
+
+    assert(x ==7); // Assertion failed: (x ==7), function main, file assert.cc, line 11.
+
+    return 0;
+
+}
