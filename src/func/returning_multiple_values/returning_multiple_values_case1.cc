295b87753b0502b930023cef5e03d7687a2c3802
aierui <aieruishi@gmail.com>
2021-07-03 14:25:56 +0800

    c

---
+++ src/func/returning_multiple_values/returning_multiple_values_case1.cc
@@ -0,0 +1,33 @@
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+struct returning_multiple_values_case1
+{
+    /* data */
+    string name;
+    int num;
+};
+
+returning_multiple_values_case1 g()
+{
+    // string t{"hello"}; // error: expected ';' at end of declaration
+    // int u{43};
+
+    string t = "hello";
+    int u = 43;
+
+    return {t, u};
+
+    // return {"hello", 10};
+}
+
+int main()
+{
+    returning_multiple_values_case1 s = g();
+
+    cout << "name: " << s.name << ", num: " << s.num << endl; // name: hello, num: 43
+
+    return 0;
+}
