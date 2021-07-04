a32a0f8a504bcca853501a8be1a429fa49b0dfe7
aierui <aieruishi@gmail.com>
2021-07-04 16:10:41 +0800

    c

---
+++ src/array/heap.cc
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+void do_something(size_t size)
+{
+    // Declare an array of doubles to be allocated on the [heap]
+    double *numbers = new double[size]{0};
+
+    // Assign a new value to the first element
+    numbers[0] = 1;
+
+    // Assign a value to each subsequent element
+    // (numbers[1] is the second element in the array.)
+    for (size_t i = 1; i < size; i++)
+    {
+        numbers[i] = numbers[i - 1] * 1.1;
+    }
+
+    // Access each element with subscript operator
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << numbers[i] << " ";
+    }
+
+    // Access each element with pointer arithmetic
+    // Use a copy of the pointer for iterating
+    double *p = numbers;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        // Dereference the pointer, then increment it
+        std::cout << *p++ << " ";
+    }
+
+    // Alternate method:
+    // Reset p to numbers[0]:
+    p = numbers;
+
+    // Use address of pointer to compute bounds.
+    // The compiler computes size as the number
+    // of elements * (bytes per element).
+    while (p < (numbers + size))
+    {
+        // Dereference the pointer, then increment it
+        std::cout << *p++ << " ";
+    }
+
+    delete[] numbers; // don't forget to do this!
+}
+int main()
+{
+    do_something(108);
+}
---
+++ src/array/stack.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    constexpr size_t size = 1000;
+
+    // Declare an array of doubles to be allocated on the stack
+    double numbers[size]{0};
+
+    // Assign a new value to the first element
+    numbers[0] = 1;
+
+    // Assign a value to each subsequent element
+    // (numbers[1] is the second element in the array.)
+    for (size_t i = 1; i < size; i++)
+    {
+        numbers[i] = numbers[i - 1] * 1.1;
+    }
+
+    // Access each element
+    for (size_t i = 0; i < size; i++)
+    {
+        std::cout << numbers[i] << " ";
+    }
+}
